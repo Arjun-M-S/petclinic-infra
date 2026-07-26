a6776bb37bb2cd199bb3ec35b3995233f6239a3f
Arjun-MS2005 <arjunms2005@gmail.com>
2026-07-26 22:45:12 +0530

    changed kubernetes version

--- eks.tf
+++ eks.tf
@@ -7,7 +7,7 @@ module "eks" {
   source                 = "terraform-aws-modules/eks/aws"
   version                = "21.0.1"
   name                   = "petclinic-eks-cluster"
-  kubernetes_version     = "1.30"
+  kubernetes_version     = "1.36"
   subnet_ids             = module.vpc.private_subnets
   vpc_id                 = module.vpc.vpc_id
   endpoint_public_access = true
