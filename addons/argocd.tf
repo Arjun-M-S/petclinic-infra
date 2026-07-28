ca570de1954484c94c281b319f7b06e34bd6ea43
Arjun MS <108624770+Arjun-M-S@users.noreply.github.com>
2026-07-28 10:38:58 +0530

    Remove petclinic application manifest
    Removed the petclinic application manifest from ArgoCD configuration.

--- addons/argocd.tf
+++ addons/argocd.tf
@@ -25,37 +25,3 @@ resource "helm_release" "argocd" {
 
   depends_on = [kubernetes_namespace.argocd]
 }
-
-resource "kubernetes_manifest" "petclinic_app" {
-  manifest = {
-    apiVersion = "argoproj.io/v1alpha1"
-    kind       = "Application"
-    metadata = {
-      name      = "petclinic"
-      namespace = kubernetes_namespace.argocd.metadata[0].name
-    }
-    spec = {
-      project = "default"
-      source = {
-        repoURL        = "https://github.com/Arjun-M-S/GitOps.git"
-        targetRevision = "main"
-        path           = "."
-      }
-      destination = {
-        server    = "https://kubernetes.default.svc"
-        namespace = "default"
-      }
-      syncPolicy = {
-        automated = {
-          prune    = true
-          selfHeal = true
-        }
-        syncOptions = [
-          "CreateNamespace=true"
-        ]
-      }
-    }
-  }
-
-  depends_on = [helm_release.argocd]
-}
